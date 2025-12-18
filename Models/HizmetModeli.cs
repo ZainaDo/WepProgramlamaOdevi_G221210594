d27e33a49f1cfd5a9c99346e7a4151dd648bdfc6
Zaina <zenah.aldous22@gmail.com>
2025-12-18 22:37:12 +0300

    Add files via upload

---
+++ Models/HizmetModeli.cs
@@ -0,0 +1,13 @@
+﻿namespace WebProjesi.Models
+{
+    public class HizmetModeli //CRUD kullandik
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Aciklama { get; set; }
+        public int Sure { get; set; } // dakika
+        public decimal Fiyat { get; set; }
+        public string Saat { get; set; }
+        public string Resim { get; set; }
+    }
+}
